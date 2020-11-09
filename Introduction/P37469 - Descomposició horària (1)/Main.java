4ab057db0bde7053e24c4dbf38016cb93fece463
cmasana <4097499+cmasana@users.noreply.github.com>
2020-11-09 23:07:49 +0100

    Update Main.java

--- Introduction/P37469 - Descomposició horària (1)/Main.java
+++ Introduction/P37469 - Descomposició horària (1)/Main.java
@@ -17,24 +17,36 @@ public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in); // Declaramos scanner
 
-        // Declaramos variable y asignamos valor introducido por consola
+        /*
+        Declaramos variable y asignamos valor introducido por consola
+        */
         int tiempo = teclado.nextInt();
         
-        // Declaramos constantes para almacenar unidades de tiempo
+        /*
+        Declaramos constantes para almacenar unidades de tiempo
+        */
         final int HORA = 3600; // Segundos que tiene 1 hora
         final int MINUTO = 60; // Segundos que tiene 1 minuto
         
-        // Declaramos variable que almacena las horas totales
+        /*
+        Declaramos variable que almacena las horas totales
+        */
         int tHoras = tiempo / HORA;
         
-        // Asignamos nuevo valor a variable tiempo 
+        /*
+        Asignamos nuevo valor a variable tiempo 
+        */
         tiempo = tiempo % HORA;
         
-        // Declaramos variables que almacenan los minutos y segundos totales
+        /*
+        Declaramos variables que almacenan los minutos y segundos totales
+        */
         int tMins = tiempo / MINUTO;
         int tSegs = tiempo % MINUTO;
         
-        // Mostramos resultado por pantalla
+        /*
+        Mostramos resultado por pantalla, concatenando los resultados.
+        */
         System.out.println(tHoras + " " + tMins + " " + tSegs);
     }
 }
