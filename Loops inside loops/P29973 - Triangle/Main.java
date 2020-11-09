d48db3f71fce97b780d7e9fe6f5128d5e3bd6a23
cmasana <4097499+cmasana@users.noreply.github.com>
2020-11-09 21:50:11 +0100

    Update Main.java

--- Loops inside loops/P29973 - Triangle/Main.java
+++ Loops inside loops/P29973 - Triangle/Main.java
@@ -13,34 +13,63 @@ public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in); // Declaramos scanner
 
-        int num; // Almacenamos nº desde teclado
-        num = teclado.nextInt();
+        /*
+        Asignamos a variable num el entero intrucido por el usuario
+         */
+        int num = teclado.nextInt();
+
+        /*
+        Almacena el nº de filas
+        */
         int filas = 0;
+
+        /*
+        Almacena el nº de columnas
+         */
         int columnas = 0;
+
+        /*
+        Almacena una cadena de caracteres vacía para evitar errores de presentación en Jutge
+         */
         String asterisco = "";
-        
-        // Bucle para las filas
+
+        /*
+        Mientras el número de filas sea menor que el numero introducido se repetirá el bucle
+         */
         while(filas < num){
-            // Bucle para cada línea de la fila
-            while(columnas <= filas){ // La primera vez 'i' = 'j' así que no entra al bucle
+            /*
+            Este otro bucle evalúa las columnas, así que mientras el número de columnas sea menor o igual que el
+            nº de filas se rpetirá el bucle
+             */
+            while(columnas <= filas){
+
+                /*
+                Añadimos a nuestra variable asterisco un *
+                 */
                 asterisco += "*";
+
+                /*
+                Incrementamos el valor de las columnas en 1
+                 */
                 columnas++;
             }
+
+            /*
+            Mostramos por pantalla el resultado de la primera fila. Básicamente sería Primera fila tiene 1 columna
+             */
             System.out.println(asterisco);
+
+            /*
+            Incrementamos el valor de la variable filas para pasar a la siguiente fila
+             */
             filas++;
-            columnas = 0; // Reiniciamos valor de la variable para cada fila
+            
+            /*
+            Es necesario reinicializar la variable columnas y la de asterisco porque sino no se pintarán los asteriscos
+            correspondientes para cada una de las filas
+             */
+            columnas = 0;
             asterisco = "";
         }
-        
-        /*
-        // Bucle para las filas
-        for (int i = 0; i < num; i++) {
-            // Bucle para cada línea de la fila
-            for (int j = 0; j < i; j++) { // La primera vez 'i' = 'j' así que no entra al bucle
-                System.out.print("*");
-            }
-            System.out.println("*");
-        }
-        */
     }
 }
