a9d42285ee696bd10208e4d4c66000fd5e7cafbb
cmasana <4097499+cmasana@users.noreply.github.com>
2020-11-09 15:48:47 +0100

    Update Main.java

--- First Loops/P69277 - Primers cubs/Main.java
+++ First Loops/P69277 - Primers cubs/Main.java
@@ -16,14 +16,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in); // Declaramos scanner
 
-        // Declaramos variable y asignamos valor introducido por consola
+        /*
+        Declaramos y asignamos a la variable n el valor introducido por consola
+        */
         int n = teclado.nextInt();
         
-        // Bucle que imprime los cubos
+        /*
+        Bucle que imprime los cubos
+        i = 0 porque necesitamos que el primer valor obtenido sea 0
+        i menor que n porque necesitamos que el bucle no tome en cuenta el numero introducido
+        y así evitar que se muestre una última coma final
+        Incrementamos el valor de i de uno en uno
+        */
         for(int i = 0; i < n; i++) {
-            System.out.print((i * i * i) + ","); // Formato con coma hasta n - 4
+            System.out.print((i * i * i) + ","); // Formato con coma
         }
         
+        /*
+        Añadimos al resultado anterior el cubo de n y cambiamos así
+        el formato para que no se muestre la coma final
+        */
         System.out.println(n * n * n); // Formato sin coma final
     }
 }
