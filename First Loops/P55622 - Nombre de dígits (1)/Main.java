f92e44bf183a836abcb13aeee05243f668668b67
cmasana <4097499+cmasana@users.noreply.github.com>
2020-11-09 15:33:06 +0100

    Update Main.java

--- First Loops/P55622 - Nombre de dígits (1)/Main.java
+++ First Loops/P55622 - Nombre de dígits (1)/Main.java
@@ -15,11 +15,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in); // Declaramos scanner
 
-        // Declaramos variable y asignamos valor introducido por consola
+        /*
+        Declaramos variable n y asignamos valor introducido por consola
+        */
         int n = teclado.nextInt(); // Almacena el número
-        int digitos = 0;
+        int digitos = 0; // Almacena el nº de dígitos de un nº
         
-        // Bucle para contar los digitos de n
+        /*
+        Asignamos a i el valor del número n 
+        Mientras i siga siendo mayor que 0 se podrán seguir realizando las operaciones
+        Cada vez que se ejecuta el bucle sumamos 1 a la variable digito
+        Dentro ya del bucle, dividimos entre 10 para obtener así el nº de digitos
+        e inicializamos la variable i con lo sobrante para no obtener un bucle infinito
+        */
         for(int i = n; i > 0; digitos++) {
             i = i / 10; // Cada vez que se divide entre 10, tenemos 1 dígito
         }
