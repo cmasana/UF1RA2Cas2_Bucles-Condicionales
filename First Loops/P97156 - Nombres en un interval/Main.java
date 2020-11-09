bb0928b981e04d2099b657bae24e7f67688cd3b5
cmasana <4097499+cmasana@users.noreply.github.com>
2020-11-09 16:11:32 +0100

    Update Main.java

--- First Loops/P97156 - Nombres en un interval/Main.java
+++ First Loops/P97156 - Nombres en un interval/Main.java
@@ -16,18 +16,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in); // Declaramos scanner
 
-        // Declaramos variable y asignamos valor introducido por consola
+        /*
+        Declaramos variables y asignamos valores introducidos por consola
+        */
         int a = teclado.nextInt();
         int b = teclado.nextInt();
         
-        // Creamos condicional para verificar que a sea mayor o igual que b
-        if(a <= b){
-            // Bucle que imprime los números entre a y b
+        /*
+        Creamos condicional para evaluar las diferentes casuisticas
+        */
+        if(a <= b){ // Si a es mayor o igual a b
+            /*
+            Asignamos a i el valor de a (por ser este el mayor)
+            Mientras i sea menor que b se repetirá el bucle, ya que no queremos 
+            mostrar una coma final
+            Incrementamos el valor de i de 1 en 1
+            */
             for (int i = a; i < b; i++) {
                 System.out.print(i + ","); // Formato para números entre a y b
             }
             System.out.println(b); // Imprime último número (sin coma)
         }
+        /*
+        En el resto de casos mostramos una cadena vacía
+        */
         else {
             System.out.println("");
         }
