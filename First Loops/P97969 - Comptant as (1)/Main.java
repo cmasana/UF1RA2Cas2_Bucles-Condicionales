3f309ed9cbbd21e8d6f7e19078eff994232b757c
cmasana <4097499+cmasana@users.noreply.github.com>
2020-11-09 16:17:25 +0100

    Update Main.java

--- First Loops/P97969 - Comptant as (1)/Main.java
+++ First Loops/P97969 - Comptant as (1)/Main.java
@@ -16,22 +16,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in); // Declaramos scanner
 
-        // Declaramos variable y asignamos valor introducido por consola
+        /*
+        Declaramos variable y asignamos valor introducido por consola
+        */
         String cadena = teclado.nextLine();
         
-        int contLetras = 0; // Almacena cuantas veces se encuentra letra 'a'
+        /*
+        Variable que almacena cuantas veces se encuentra letra 'a'
+        */
+        int contLetras = 0;
         
-        // Recorremos cadena con un bucle
+        /*
+        Recorremos la cadena con un bucle
+        i = 0 porque cuando utilizamos caracteres la primera posición se corresponde con un 0
+        y necesitamos evaluar si en la primera letra hay una 'a' también
+        
+        El bucle se repite mientras i sea menor a la longitud de la cadena
+        
+        Incrementamos el valor de i de uno en 1 para que vaya recorriendo cada uno
+        de los caracteres
+        */
         for(int i = 0; i < cadena.length(); i++){
             
-            // Evaluamos caracter por caracter. Si c/u contiene la letra 'a'
+            /*
+            Evaluamos caracter por caracter. Si c/u contiene la letra 'a'
+            sumamos 1 al contador
+            */
             if(cadena.charAt(i) == 'a'){
-                contLetras++; // Sumamos 1 al contador
+                contLetras++;
             }
         }
-        
-        // Imprimimos valor por pantalla
-        System.out.println(contLetras);
+       
+        System.out.println(contLetras); // Imprimimos resultado por pantalla
         
     }
 }
