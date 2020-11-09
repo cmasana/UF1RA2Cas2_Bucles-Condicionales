9ab6bd9ad6304fbd047806937f93c02f93a5af9a
cmasana <4097499+cmasana@users.noreply.github.com>
2020-11-09 16:05:41 +0100

    Update Main.java

--- First Loops/P78142 - Mitjana/Main.java
+++ First Loops/P78142 - Mitjana/Main.java
@@ -17,24 +17,65 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in); // Declaramos scanner
-        Locale.setDefault(new Locale("en", "US")); // Añadimos punto como separador decimal
-
-        double num; // Almacena numeros introducidos
-        int contador = 0; // Cuenta los numeros introducidos para hallar la media
-        double resultado; // Almacena el resultado en decimales
-        double suma = 0; // Almacena la suma de los números introducidos
         
+        /*
+        Utilizamos la clase Locale de java para hacer un cambio de cultura/región en
+        nuestro programa y establecer así que los números decimales utilicen como separador,
+        por defecto,un punto y no una coma. (Si no lo hacemos Jutge muestra un error de 
+        presentación)
+        */
+        Locale.setDefault(new Locale("en", "US")); 
+
+        /*
+        Almacena números con coma flotante introducidos 
+        */
+        double num;
+        
+        /*
+        Cuenta el total de numeros introducidos para hallar la media
+        */
+        int contador = 0;
+        
+        /*
+        Almacena el resultado (la media)
+        */
+        double resultado;
+        
+        /*
+        Almacena la suma de los números introducidos
+        */
+        double suma = 0;
+        
+        /*
+        Bucle que se ejecuta mientras haya números introducidos
+        Esto permite que se puedan introducir un número indeterminado de
+        números
+        */
         while(teclado.hasNextDouble()){
             num = teclado.nextDouble(); // Almacena nº introducido por teclado
-            suma += num; // Va sumando números introducidos
-            contador++; // Suma 1 al contador
+            
+            /*
+            Suma todos los números introducidos y asigna el total a la variable suma
+            */
+            suma += num;
+            
+            /*
+            Incrementamos el contador en 1 por cada número introducido
+            */
+            contador++;
         }
         
         teclado.close(); // Cerramos scanner
         
-        resultado = suma / contador; // Media de números
+        /*
+        Asignamos a la variable resultado el valor de la operación (la media)
+        */
+        resultado = suma / contador; 
         
-        // Cambiamos formato de decimales
+        /*
+        Cambiamos el formato decimal, para que muestre como máximo 2 dígitos después
+        de la coma flotante
+        */
         DecimalFormat formato = new DecimalFormat("0.00");
         
         System.out.println(formato.format(resultado)); // Imprime resultado
