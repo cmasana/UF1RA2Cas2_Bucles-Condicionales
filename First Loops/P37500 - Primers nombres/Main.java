acafffe7d9c16a8c939014ee5dfb8f2f6a7a5cfb
cmasana <4097499+cmasana@users.noreply.github.com>
2020-11-09 14:48:10 +0100

    Update Main.java

--- First Loops/P37500 - Primers nombres/Main.java
+++ First Loops/P37500 - Primers nombres/Main.java
@@ -16,10 +16,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in); // Declaramos scanner
 
-        // Declaramos variable y asignamos valor introducido por consola
+        /*
+        Declaramos variable numero y asignamos valor introducido por consola
+        */
         int numero = teclado.nextInt();
         
-        // Bucle que imprime resultados
+        /*
+        Bucle que imprime los resultados
+        i = 0 porque queremos mostrar los números desde 0
+        i menor o igual a numero porque el bucle tiene que repetirse desde 0 hasta el nº
+        introducido por el usuario (incluido)
+        Aumentamos el valor de i de 1 en 1 para imprimir cada nº
+        */
         for (int i = 0; i <= numero; i++){
             System.out.println(i);
         }  
